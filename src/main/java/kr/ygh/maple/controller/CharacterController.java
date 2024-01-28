863801cee8e9d4b45b2f413334262a6bc71a15b3
Seyang <chchch1213@naver.com>
2024-01-29 00:45:54 +0900

    Start at basic api server

--- src/main/java/kr/ygh/maple/controller/CharacterController.java
+++ src/main/java/kr/ygh/maple/controller/CharacterController.java
@@ -1,19 +1,30 @@
 package kr.ygh.maple.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
-import java.util.HashMap;
 import java.util.Map;
 
-@RestController
 @RequestMapping("/character")
 public class CharacterController {
 
+    @Value("${nexon.api.url}")
+    private String NEXON_API_URL;
+    @Value("${nexon.api.key}")
+    private String NEXON_API_KEY;
+
     @GetMapping("item-equipment")
-    public Map<String, Object> itemEquipment(Map<String, Object> req) {
-        Map<String, Object> result = new HashMap<>();
-        return result;
+    public Mono<String> itemEquipment(Map<String, Object> req) {
+        System.out.println(req.get("character_name").toString());
+        return WebClient.builder()
+                .baseUrl(NEXON_API_URL + "/maplestory/v1/id?character_name=" + req.get("character_name"))
+                .defaultHeader("x-nxopen-api-key", NEXON_API_URL)
+                .build()
+                .get()
+                .retrieve()
+                .bodyToMono(String.class);
     }
 }
