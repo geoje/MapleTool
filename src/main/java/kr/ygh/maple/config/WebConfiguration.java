b958643af6c9c6f0f697e52aca92c0dabf113c68
Gyeongho Yang <chchch1213@naver.com>
2024-01-30 00:29:32 +0900

    Add redirection all path to root for react router

---
+++ src/main/java/kr/ygh/maple/config/WebConfiguration.java
@@ -0,0 +1,14 @@
+package kr.ygh.maple.config;
+
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/{spring:\\w+}").setViewName("forward:/");
+        registry.addViewController("/**/{spring:\\w+}").setViewName("forward:/");
+        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}").setViewName("forward:/");
+        WebMvcConfigurer.super.addViewControllers(registry);
+    }
+}
