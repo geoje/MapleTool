523bff39912be1f84c82a860d17bb135e9ad1902
Seyang <chchch1213@naver.com>
2024-01-29 17:29:22 +0900

    Add api ocid and basic

--- src/main/java/kr/ygh/maple/controller/CharacterController.java
+++ src/main/java/kr/ygh/maple/controller/CharacterController.java
@@ -1,14 +1,19 @@
 package kr.ygh.maple.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 @RestController
 @RequestMapping("/api/character")
@@ -19,25 +24,43 @@ public class CharacterController {
     @Value("${nexon.api.key}")
     private String NEXON_API_KEY;
 
-    @GetMapping("test")
-    public Map<String, Object> test() {
-        System.out.println("NEXON_API_URL = " + NEXON_API_URL);
-        System.out.println("NEXON_API_KEY = " + NEXON_API_KEY);
-
-        HashMap<String, Object> map = new HashMap<String, Object>();
-        map.put("hello", "world");
-        return map;
+    private static Mono<? extends Throwable> handleError(ClientResponse response) {
+        return response.bodyToMono(ErrorResponse.class)
+                .flatMap(error -> Mono.error(new ResponseStatusException(response.statusCode(), response.toString())));
     }
 
-    @GetMapping("item-equipment")
-    public Mono<String> itemEquipment(Map<String, Object> req) {
-        System.out.println("req.get(\"character_name\") = " + req.get("character_name"));
-        return WebClient.builder()
-                .baseUrl(NEXON_API_URL + "/maplestory/v1/id?character_name=" + req.get("character_name"))
-                .defaultHeader("x-nxopen-api-key", NEXON_API_KEY)
-                .build()
-                .get()
+    @GetMapping("ocid")
+    public Mono<String> ocid(@RequestParam("character_name") String name) {
+
+        if (name.trim().isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Query character_name is blank or empty");
+        }
+
+        return WebClient.create(NEXON_API_URL).get()
+                .uri("/maplestory/v1/id?character_name={name}", name)
+                .header("x-nxopen-api-key", NEXON_API_KEY)
                 .retrieve()
                 .bodyToMono(String.class);
+    }
+
+    @GetMapping("basic")
+    public Mono<String> basic(@RequestParam("ocid") String ocid) {
+
+        if (ocid.trim().isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Query ocid is blank or empty");
+        }
+
+        return WebClient.create(NEXON_API_URL).get()
+                .uri("/maplestory/v1/character/basic?ocid={ocid}&date={date}",
+                     ocid,
+                     LocalDate.now(ZoneId.of("Asia/Seoul")).minusDays(1).toString()
+                )
+                .header("x-nxopen-api-key", NEXON_API_KEY)
+                .retrieve()
+//                .onStatus(HttpStatus::isError, CharacterController::handleError)
+                .bodyToMono(String.class)
+                .doOnError(throwable -> {
+                    throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, throwable.getMessage());
+                });
     }
 }
--- src/test/java/kr/ygh/maple/MapleToolApplicationTests.java
+++ src/test/java/kr/ygh/maple/MapleToolApplicationTests.java
@@ -3,11 +3,19 @@ package kr.ygh.maple;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+
 @SpringBootTest
 class MapleToolApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
+    @Test
+    void printDateKST() {
+        LocalDate yesterday = LocalDate.now(ZoneId.of("Asia/Seoul")).minusDays(1);
+        System.out.println("today = " + yesterday);
+    }
 }
