f6b482c78ac6c4d3b10dbfcad5142742422f0ede
Seyang <chchch1213@naver.com>
2024-01-29 10:50:37 +0900

    Need to fix value annotation to get properties

--- src/main/java/kr/ygh/maple/controller/CharacterController.java
+++ src/main/java/kr/ygh/maple/controller/CharacterController.java
@@ -3,12 +3,15 @@ package kr.ygh.maple.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.util.HashMap;
 import java.util.Map;
 
-@RequestMapping("/character")
+@RestController
+@RequestMapping("/api/character")
 public class CharacterController {
 
     @Value("${nexon.api.url}")
@@ -16,12 +19,19 @@ public class CharacterController {
     @Value("${nexon.api.key}")
     private String NEXON_API_KEY;
 
+    @GetMapping("test")
+    public Map<String, Object> test() {
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("hello", "world");
+        return map;
+    }
+
     @GetMapping("item-equipment")
     public Mono<String> itemEquipment(Map<String, Object> req) {
-        System.out.println(req.get("character_name").toString());
+        System.out.println("req.get(\"character_name\") = " + req.get("character_name"));
         return WebClient.builder()
                 .baseUrl(NEXON_API_URL + "/maplestory/v1/id?character_name=" + req.get("character_name"))
-                .defaultHeader("x-nxopen-api-key", NEXON_API_URL)
+                .defaultHeader("x-nxopen-api-key", NEXON_API_KEY)
                 .build()
                 .get()
                 .retrieve()
