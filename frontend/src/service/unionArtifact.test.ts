b1a64f0e96f4ff22192966fd86f3c70d70df3b07
Gyeongho Yang <chchch1213@naver.com>
2024-02-17 14:45:29 +0900

    feat: Success test initally

--- frontend/src/service/unionArtifact.test.ts
+++ frontend/src/service/unionArtifact.test.ts
@@ -1,6 +1,6 @@
 import UnionArtifactService from "./unionArtifact";
 
 test("demo", () => expect(true).toBe(true));
-it("add correctly", () => {
+test("add correctly", () => {
   expect(UnionArtifactService.add(3, 5)).toBe(8);
 });
