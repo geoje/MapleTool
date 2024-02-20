513028baa9d344423f2123240aec5c7644f9ee89
Gyeongho Yang <chchch1213@naver.com>
2024-02-21 00:08:04 +0900

    Remove nullable on all domain

--- frontend/src/domain/union.ts
+++ frontend/src/domain/union.ts
@@ -1,56 +1,56 @@
 export interface Union {
-  basic?: UnionBasic;
-  artifact?: UnionArtifact;
-  raider?: UnionRaider;
+  basic: UnionBasic;
+  artifact: UnionArtifact;
+  raider: UnionRaider;
 }
 
 export interface UnionBasic {
-  date?: string;
-  union_level?: number;
-  union_grade?: string;
-  artifact_level?: number;
-  artifact_exp?: number;
-  artifact_point?: number;
+  date: string;
+  union_level: number;
+  union_grade: string;
+  artifact_level: number;
+  artifact_exp: number;
+  artifact_point: number;
 }
 
 export interface UnionArtifact {
-  date?: string;
-  union_artifact_effect?: UnionArtifactEffect[];
-  union_artifact_crystal?: UnionArtifactCrystal[];
+  date: string;
+  union_artifact_effect: UnionArtifactEffect[];
+  union_artifact_crystal: UnionArtifactCrystal[];
 }
 export interface UnionArtifactEffect {
-  name?: string;
-  level?: number;
+  name: string;
+  level: number;
 }
 export interface UnionArtifactCrystal {
-  name?: string;
-  validity_flag?: string;
-  date_expire?: string;
-  level?: number;
-  crystal_option_name1?: string;
-  crystal_option_name2?: string;
-  crystal_option_name3?: string;
+  name: string;
+  validity_flag: string;
+  date_expire: string;
+  level: number;
+  crystal_option_name1: string;
+  crystal_option_name2: string;
+  crystal_option_name3: string;
 }
 
 export interface UnionRaider {
-  date?: string;
-  union_raider_stat?: string[];
-  union_occupied_stat?: string[];
-  union_inner_stat?: UnionRaiderStat[];
-  union_block?: UnionRaiderBlock[];
+  date: string;
+  union_raider_stat: string[];
+  union_occupied_stat: string[];
+  union_inner_stat: UnionRaiderStat[];
+  union_block: UnionRaiderBlock[];
 }
 export interface UnionRaiderStat {
-  stat_field_id?: string;
-  stat_field_effect?: string;
+  stat_field_id: string;
+  stat_field_effect: string;
 }
 export interface UnionRaiderBlock {
-  block_type?: string;
-  block_class?: string;
-  block_level?: string;
-  block_control_point?: UnionRaiderBlockPoint;
-  block_position?: UnionRaiderBlockPoint[];
+  block_type: string;
+  block_class: string;
+  block_level: string;
+  block_control_point: UnionRaiderBlockPoint;
+  block_position: UnionRaiderBlockPoint[];
 }
 export interface UnionRaiderBlockPoint {
-  x?: number;
-  y?: number;
+  x: number;
+  y: number;
 }
