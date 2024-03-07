8fbe22ea4f8f2d1e39ee149f4f16e6ba00b36ce5
Gyeongho Yang <chchch1213@naver.com>
2024-03-07 23:23:40 +0900

    refactor: Adjust chunkSizeWarningLimit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
   server: {
     host: true,
     watch: {
