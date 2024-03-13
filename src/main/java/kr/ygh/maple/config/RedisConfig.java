0aee78dbbab7e9874d11cf1619a127eff91eb0c7
Gyeongho Yang <chchch1213@naver.com>
2024-03-14 01:53:16 +0900

    fix: Edit redis host and port on config production code

--- src/main/java/kr/ygh/maple/config/RedisConfig.java
+++ src/main/java/kr/ygh/maple/config/RedisConfig.java
@@ -10,10 +10,10 @@ import org.springframework.data.redis.core.RedisTemplate;
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.redis.host}")
+    @Value("${spring.data.redis.host}")
     private String host;
 
-    @Value("${spring.redis.port}")
+    @Value("${spring.data.redis.port}")
     private int port;
 
     @Bean
