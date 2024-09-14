8f8fa29d5ce7889719dd8dd3da765258e4ea4ee6
Gyeongho Yang <chchch1213@naver.com>
2024-09-15 00:08:59 +0900

    feat: add index on potential table

--- backend/src/main/java/kr/ygh/maple/character/entity/Potential.java
+++ backend/src/main/java/kr/ygh/maple/character/entity/Potential.java
@@ -2,8 +2,13 @@ package kr.ygh.maple.character.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(indexes = {
+        @Index(name = "idx_type_part_grade_level", columnList = "type, part, grade, level")
+})
 public record Potential(
         @Id long id,
         String type, String part, String grade, int level,
