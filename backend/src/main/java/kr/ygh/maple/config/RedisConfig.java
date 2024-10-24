e4ba45ab25fa22ebf1386485fa06f77436e265e8
Gyeongho Yang <chchch1213@naver.com>
2024-10-25 02:23:49 +0900

    feat: add dynamic ttl on cache

--- backend/src/main/java/kr/ygh/maple/config/RedisConfig.java
+++ backend/src/main/java/kr/ygh/maple/config/RedisConfig.java
@@ -1,6 +1,10 @@
 package kr.ygh.maple.config;
 
+import jakarta.annotation.Nullable;
+import java.time.DayOfWeek;
 import java.time.Duration;
+import java.time.LocalDateTime;
+import kr.ygh.maple.character.dto.itemEquipment.PotentialRequest;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,11 +18,11 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @EnableCaching
 @Configuration
 public class RedisConfig {
-    
+
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
         RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(10))
+                .entryTtl(this::calculateTtl)
                 .serializeKeysWith(
                         RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
@@ -27,5 +31,29 @@ public class RedisConfig {
         return RedisCacheManager.builder(redisConnectionFactory)
                 .cacheDefaults(configuration)
                 .build();
+    }
+
+    private Duration calculateTtl(Object key, @Nullable Object value) {
+        if (key.getClass() == PotentialRequest.class) {
+            return Duration.ZERO;
+        }
+        if (value == null) {
+            return Duration.ofMinutes(15);
+        }
+        if (value.getClass() == String.class) {
+            return getUntilNextPatch();
+        }
+
+        return Duration.ofMinutes(15);
+    }
+
+    private Duration getUntilNextPatch() {
+        LocalDateTime now = LocalDateTime.now();
+
+        if (now.getDayOfWeek() == DayOfWeek.THURSDAY && now.getHour() < 7) {
+            return Duration.between(now, now.withHour(7));
+        }
+
+        return Duration.between(now, now.toLocalDate().plusDays(1).atStartOfDay());
     }
 }
