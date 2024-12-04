7623b4b6048f5afcb0b674f240d9cd0dad07915e
Gyeongho Yang <chchch1213@naver.com>
2024-12-05 00:16:41 +0900

    feat: setup zdd with k6

--- server/test/check.js
+++ server/test/check.js
@@ -1,15 +1,19 @@
 import http from "k6/http";
 import { check } from "k6";
 
-export let options = { vus: 1000, duration: "10s", rps: 1000 };
+export const options = { vus: 100, duration: "1m", rps: 100 };
 
 export default function () {
-  let res = http.get(
-    "http://be:8080/api/character/item-equipment/potentials?type=%EB%B8%94%EB%9E%99&part=%EB%AC%B4%EA%B8%B0&level=200"
+  const res = http.get(
+    "http://be:8080" +
+      "/api/character/item-equipment/potentials" +
+      "?type=%EB%B8%94%EB%9E%99&part=%EB%AC%B4%EA%B8%B0&level=200"
   );
 
   check(res, {
     "is status 200": (r) => r.status === 200,
-    "response time is less than 1000ms": (r) => r.timings.duration < 1000,
+    // "response time is less than 1000ms": (r) => r.timings.duration < 1000,
   });
+
+  console.log(res.status_text, res.body.length);
 }
