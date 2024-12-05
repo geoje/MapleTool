6ada36c66d6fe1653ddd580ebf1c5b17e389fe24
Gyeongho Yang <chchch1213@naver.com>
2024-12-05 09:40:45 +0900

    feat: add fe zdd

--- server/script/restart.bat
+++ server/script/restart.bat
@@ -6,3 +6,4 @@ for /f "tokens=1,2 delims==" %%a in (../docker/.env) do (
 
 docker stack down mapletool
 docker stack deploy -c ../docker/docker-compose-be.yaml mapletool
+docker stack deploy -c ../docker/docker-compose-fe.yaml mapletool
--- server/script/update.bat
+++ server/script/update.bat
@@ -1,1 +1,4 @@
+@echo off
+
 docker service update --force mapletool_be
+docker service update --force mapletool_fe
