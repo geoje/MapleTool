ff39d7d38aad8787a04978d53028ce0456f0a3c9
Gyeongho Yang <chchch1213@naver.com>
2024-12-05 09:10:57 +0900

    refactor: move docker and script to inner dir

---
+++ server/script/restart.bat
@@ -0,0 +1,8 @@
+@echo off
+
+for /f "tokens=1,2 delims==" %%a in (../docker/.env) do (
+    set %%a=%%b
+)
+
+docker stack down mapletool
+docker stack deploy -c ../docker/docker-compose-be.yaml mapletool
