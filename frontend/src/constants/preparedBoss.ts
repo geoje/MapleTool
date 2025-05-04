981dc5c5d0e88dac955a139bcf0a8d62dd431315
Gyeongho Yang <chchch1213@naver.com>
2025-05-04 20:47:45 +0200

    feat: add prepared boss

--- frontend/src/constants/preparedBoss.ts
+++ frontend/src/constants/preparedBoss.ts
@@ -5,62 +5,105 @@ interface bossItem {
   difficulty: BOSS_DIFFICULTY;
 }
 
-export const LOTUS_DAMIEN: bossItem[] = [
-  { type: BOSS_TYPE.ZZAKUM, difficulty: BOSS_DIFFICULTY.CHAOS },
-  { type: BOSS_TYPE.MAGNUS, difficulty: BOSS_DIFFICULTY.HARD },
-  { type: BOSS_TYPE.HILLA, difficulty: BOSS_DIFFICULTY.HARD },
-  { type: BOSS_TYPE.PAPULATUS, difficulty: BOSS_DIFFICULTY.CHAOS },
-  { type: BOSS_TYPE.PIERRE, difficulty: BOSS_DIFFICULTY.CHAOS },
-  { type: BOSS_TYPE.VON_BON, difficulty: BOSS_DIFFICULTY.CHAOS },
-  { type: BOSS_TYPE.CRIMSON_QUEEN, difficulty: BOSS_DIFFICULTY.CHAOS },
-  { type: BOSS_TYPE.VELLUM, difficulty: BOSS_DIFFICULTY.CHAOS },
-  { type: BOSS_TYPE.PINK_BEAN, difficulty: BOSS_DIFFICULTY.CHAOS },
-  { type: BOSS_TYPE.CYGNUS, difficulty: BOSS_DIFFICULTY.NORMAL },
-  { type: BOSS_TYPE.LOTUS, difficulty: BOSS_DIFFICULTY.NORMAL },
-  { type: BOSS_TYPE.DAMIEN, difficulty: BOSS_DIFFICULTY.NORMAL },
-];
-
-export const GUARDIAN_ANGEL_SLIME: bossItem[] = [
-  { type: BOSS_TYPE.ZZAKUM, difficulty: BOSS_DIFFICULTY.CHAOS },
-  { type: BOSS_TYPE.MAGNUS, difficulty: BOSS_DIFFICULTY.HARD },
-  { type: BOSS_TYPE.PAPULATUS, difficulty: BOSS_DIFFICULTY.CHAOS },
-  { type: BOSS_TYPE.PIERRE, difficulty: BOSS_DIFFICULTY.CHAOS },
-  { type: BOSS_TYPE.VON_BON, difficulty: BOSS_DIFFICULTY.CHAOS },
-  { type: BOSS_TYPE.CRIMSON_QUEEN, difficulty: BOSS_DIFFICULTY.CHAOS },
-  { type: BOSS_TYPE.VELLUM, difficulty: BOSS_DIFFICULTY.CHAOS },
-  { type: BOSS_TYPE.PINK_BEAN, difficulty: BOSS_DIFFICULTY.CHAOS },
-  { type: BOSS_TYPE.CYGNUS, difficulty: BOSS_DIFFICULTY.NORMAL },
-  { type: BOSS_TYPE.LOTUS, difficulty: BOSS_DIFFICULTY.NORMAL },
-  { type: BOSS_TYPE.DAMIEN, difficulty: BOSS_DIFFICULTY.NORMAL },
-  { type: BOSS_TYPE.GUARDIAN_ANGEL_SLIME, difficulty: BOSS_DIFFICULTY.NORMAL },
-];
-
-export const EASY_LUCID_WILL: bossItem[] = [
-  { type: BOSS_TYPE.ZZAKUM, difficulty: BOSS_DIFFICULTY.CHAOS },
-  { type: BOSS_TYPE.MAGNUS, difficulty: BOSS_DIFFICULTY.HARD },
-  { type: BOSS_TYPE.PAPULATUS, difficulty: BOSS_DIFFICULTY.CHAOS },
-  { type: BOSS_TYPE.PIERRE, difficulty: BOSS_DIFFICULTY.CHAOS },
-  { type: BOSS_TYPE.VON_BON, difficulty: BOSS_DIFFICULTY.CHAOS },
-  { type: BOSS_TYPE.CRIMSON_QUEEN, difficulty: BOSS_DIFFICULTY.CHAOS },
-  { type: BOSS_TYPE.VELLUM, difficulty: BOSS_DIFFICULTY.CHAOS },
-  { type: BOSS_TYPE.LOTUS, difficulty: BOSS_DIFFICULTY.NORMAL },
-  { type: BOSS_TYPE.DAMIEN, difficulty: BOSS_DIFFICULTY.NORMAL },
-  { type: BOSS_TYPE.GUARDIAN_ANGEL_SLIME, difficulty: BOSS_DIFFICULTY.NORMAL },
-  { type: BOSS_TYPE.LUCID, difficulty: BOSS_DIFFICULTY.EASY },
-  { type: BOSS_TYPE.WILL, difficulty: BOSS_DIFFICULTY.EASY },
-];
-
-export const UNDER_BLACK_MAGE: bossItem[] = [
-  { type: BOSS_TYPE.MAGNUS, difficulty: BOSS_DIFFICULTY.HARD },
-  { type: BOSS_TYPE.PAPULATUS, difficulty: BOSS_DIFFICULTY.CHAOS },
-  { type: BOSS_TYPE.CRIMSON_QUEEN, difficulty: BOSS_DIFFICULTY.CHAOS },
-  { type: BOSS_TYPE.VELLUM, difficulty: BOSS_DIFFICULTY.CHAOS },
-  { type: BOSS_TYPE.LOTUS, difficulty: BOSS_DIFFICULTY.HARD },
-  { type: BOSS_TYPE.DAMIEN, difficulty: BOSS_DIFFICULTY.HARD },
-  { type: BOSS_TYPE.GUARDIAN_ANGEL_SLIME, difficulty: BOSS_DIFFICULTY.CHAOS },
-  { type: BOSS_TYPE.LUCID, difficulty: BOSS_DIFFICULTY.HARD },
-  { type: BOSS_TYPE.WILL, difficulty: BOSS_DIFFICULTY.HARD },
-  { type: BOSS_TYPE.GLOOM, difficulty: BOSS_DIFFICULTY.CHAOS },
-  { type: BOSS_TYPE.VERUS_HILLA, difficulty: BOSS_DIFFICULTY.HARD },
-  { type: BOSS_TYPE.DARKNELL, difficulty: BOSS_DIFFICULTY.HARD },
+export const PREPARED_BOSS: {
+  label: string;
+  boss: bossItem[];
+}[] = [
+  {
+    label: "스데",
+    boss: [
+      { type: BOSS_TYPE.ZZAKUM, difficulty: BOSS_DIFFICULTY.CHAOS },
+      { type: BOSS_TYPE.MAGNUS, difficulty: BOSS_DIFFICULTY.HARD },
+      { type: BOSS_TYPE.HILLA, difficulty: BOSS_DIFFICULTY.HARD },
+      { type: BOSS_TYPE.PAPULATUS, difficulty: BOSS_DIFFICULTY.CHAOS },
+      { type: BOSS_TYPE.PIERRE, difficulty: BOSS_DIFFICULTY.CHAOS },
+      { type: BOSS_TYPE.VON_BON, difficulty: BOSS_DIFFICULTY.CHAOS },
+      { type: BOSS_TYPE.CRIMSON_QUEEN, difficulty: BOSS_DIFFICULTY.CHAOS },
+      { type: BOSS_TYPE.VELLUM, difficulty: BOSS_DIFFICULTY.CHAOS },
+      { type: BOSS_TYPE.PINK_BEAN, difficulty: BOSS_DIFFICULTY.CHAOS },
+      { type: BOSS_TYPE.CYGNUS, difficulty: BOSS_DIFFICULTY.NORMAL },
+      { type: BOSS_TYPE.LOTUS, difficulty: BOSS_DIFFICULTY.NORMAL },
+      { type: BOSS_TYPE.DAMIEN, difficulty: BOSS_DIFFICULTY.NORMAL },
+    ],
+  },
+  {
+    label: "이루윌",
+    boss: [
+      { type: BOSS_TYPE.ZZAKUM, difficulty: BOSS_DIFFICULTY.CHAOS },
+      { type: BOSS_TYPE.MAGNUS, difficulty: BOSS_DIFFICULTY.HARD },
+      { type: BOSS_TYPE.PAPULATUS, difficulty: BOSS_DIFFICULTY.CHAOS },
+      { type: BOSS_TYPE.PIERRE, difficulty: BOSS_DIFFICULTY.CHAOS },
+      { type: BOSS_TYPE.VON_BON, difficulty: BOSS_DIFFICULTY.CHAOS },
+      { type: BOSS_TYPE.CRIMSON_QUEEN, difficulty: BOSS_DIFFICULTY.CHAOS },
+      { type: BOSS_TYPE.VELLUM, difficulty: BOSS_DIFFICULTY.CHAOS },
+      { type: BOSS_TYPE.LOTUS, difficulty: BOSS_DIFFICULTY.NORMAL },
+      { type: BOSS_TYPE.DAMIEN, difficulty: BOSS_DIFFICULTY.NORMAL },
+      {
+        type: BOSS_TYPE.GUARDIAN_ANGEL_SLIME,
+        difficulty: BOSS_DIFFICULTY.NORMAL,
+      },
+      { type: BOSS_TYPE.LUCID, difficulty: BOSS_DIFFICULTY.EASY },
+      { type: BOSS_TYPE.WILL, difficulty: BOSS_DIFFICULTY.EASY },
+    ],
+  },
+  {
+    label: "노듄더",
+    boss: [
+      { type: BOSS_TYPE.MAGNUS, difficulty: BOSS_DIFFICULTY.HARD },
+      { type: BOSS_TYPE.PAPULATUS, difficulty: BOSS_DIFFICULTY.CHAOS },
+      { type: BOSS_TYPE.CRIMSON_QUEEN, difficulty: BOSS_DIFFICULTY.CHAOS },
+      { type: BOSS_TYPE.VON_BON, difficulty: BOSS_DIFFICULTY.CHAOS },
+      { type: BOSS_TYPE.VELLUM, difficulty: BOSS_DIFFICULTY.CHAOS },
+      { type: BOSS_TYPE.LOTUS, difficulty: BOSS_DIFFICULTY.NORMAL },
+      { type: BOSS_TYPE.DAMIEN, difficulty: BOSS_DIFFICULTY.NORMAL },
+      {
+        type: BOSS_TYPE.GUARDIAN_ANGEL_SLIME,
+        difficulty: BOSS_DIFFICULTY.NORMAL,
+      },
+      { type: BOSS_TYPE.LUCID, difficulty: BOSS_DIFFICULTY.NORMAL },
+      { type: BOSS_TYPE.WILL, difficulty: BOSS_DIFFICULTY.NORMAL },
+      { type: BOSS_TYPE.GLOOM, difficulty: BOSS_DIFFICULTY.NORMAL },
+      { type: BOSS_TYPE.DARKNELL, difficulty: BOSS_DIFFICULTY.NORMAL },
+    ],
+  },
+  {
+    label: "검밑솔",
+    boss: [
+      { type: BOSS_TYPE.MAGNUS, difficulty: BOSS_DIFFICULTY.HARD },
+      { type: BOSS_TYPE.PAPULATUS, difficulty: BOSS_DIFFICULTY.CHAOS },
+      { type: BOSS_TYPE.CRIMSON_QUEEN, difficulty: BOSS_DIFFICULTY.CHAOS },
+      { type: BOSS_TYPE.VELLUM, difficulty: BOSS_DIFFICULTY.CHAOS },
+      { type: BOSS_TYPE.LOTUS, difficulty: BOSS_DIFFICULTY.HARD },
+      { type: BOSS_TYPE.DAMIEN, difficulty: BOSS_DIFFICULTY.HARD },
+      {
+        type: BOSS_TYPE.GUARDIAN_ANGEL_SLIME,
+        difficulty: BOSS_DIFFICULTY.CHAOS,
+      },
+      { type: BOSS_TYPE.LUCID, difficulty: BOSS_DIFFICULTY.HARD },
+      { type: BOSS_TYPE.WILL, difficulty: BOSS_DIFFICULTY.HARD },
+      { type: BOSS_TYPE.GLOOM, difficulty: BOSS_DIFFICULTY.CHAOS },
+      { type: BOSS_TYPE.VERUS_HILLA, difficulty: BOSS_DIFFICULTY.HARD },
+      { type: BOSS_TYPE.DARKNELL, difficulty: BOSS_DIFFICULTY.HARD },
+    ],
+  },
+  {
+    label: "하세이칼",
+    boss: [
+      { type: BOSS_TYPE.MAGNUS, difficulty: BOSS_DIFFICULTY.HARD },
+      { type: BOSS_TYPE.PAPULATUS, difficulty: BOSS_DIFFICULTY.CHAOS },
+      { type: BOSS_TYPE.LOTUS, difficulty: BOSS_DIFFICULTY.HARD },
+      { type: BOSS_TYPE.DAMIEN, difficulty: BOSS_DIFFICULTY.HARD },
+      {
+        type: BOSS_TYPE.GUARDIAN_ANGEL_SLIME,
+        difficulty: BOSS_DIFFICULTY.CHAOS,
+      },
+      { type: BOSS_TYPE.LUCID, difficulty: BOSS_DIFFICULTY.HARD },
+      { type: BOSS_TYPE.WILL, difficulty: BOSS_DIFFICULTY.HARD },
+      { type: BOSS_TYPE.GLOOM, difficulty: BOSS_DIFFICULTY.CHAOS },
+      { type: BOSS_TYPE.VERUS_HILLA, difficulty: BOSS_DIFFICULTY.HARD },
+      { type: BOSS_TYPE.DARKNELL, difficulty: BOSS_DIFFICULTY.HARD },
+      { type: BOSS_TYPE.CHOSEN_SEREN, difficulty: BOSS_DIFFICULTY.HARD },
+      { type: BOSS_TYPE.KALOS_THE_GUARDIAN, difficulty: BOSS_DIFFICULTY.EASY },
+    ],
+  },
 ];
--- frontend/src/pages/crystal/2-boss/preparedButtons.tsx
+++ frontend/src/pages/crystal/2-boss/preparedButtons.tsx
@@ -1,23 +1,7 @@
-import { Button, Flex, Tooltip } from "@chakra-ui/react";
+import { Button, Flex } from "@chakra-ui/react";
 import { useAppDispatch } from "../../../stores/hooks";
 import { setBossItems } from "../../../stores/userSlice";
-import {
-  EASY_LUCID_WILL,
-  GUARDIAN_ANGEL_SLIME,
-  LOTUS_DAMIEN,
-  UNDER_BLACK_MAGE,
-} from "../../../constants/preparedBoss";
-
-const items = [
-  { label: "스우 데미안 돌이", abbreviate: "스데", boss: LOTUS_DAMIEN },
-  { label: "가엔슬 돌이", abbreviate: "가엔슬", boss: GUARDIAN_ANGEL_SLIME },
-  { label: "이지 루시드 윌 돌이", abbreviate: "이루윌", boss: EASY_LUCID_WILL },
-  {
-    label: "검은 마법사 밑 솔격",
-    abbreviate: "검밑솔",
-    boss: UNDER_BLACK_MAGE,
-  },
-];
+import { PREPARED_BOSS } from "../../../constants/preparedBoss";
 
 export default function PreparedButtons({ selected }: { selected: number }) {
   const dispatch = useAppDispatch();
@@ -26,17 +10,15 @@ export default function PreparedButtons({ selected }: { selected: number }) {
 
   return (
     <Flex gap={2}>
-      {items.map((item, i) => (
-        <Tooltip key={"item-" + i} placement="top" label={item.label}>
-          <Button
-            size="xs"
-            onClick={() =>
-              dispatch(setBossItems({ index: selected, boss: item.boss }))
-            }
-          >
-            {item.abbreviate}
-          </Button>
-        </Tooltip>
+      {PREPARED_BOSS.map((item, i) => (
+        <Button
+          size="xs"
+          onClick={() =>
+            dispatch(setBossItems({ index: selected, boss: item.boss }))
+          }
+        >
+          {item.label}
+        </Button>
       ))}
     </Flex>
   );
