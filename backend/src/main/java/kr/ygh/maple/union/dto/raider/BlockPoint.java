b08141f075b520073aa3c63213dba082d33107a7
Gyeongho Yang <chchch1213@naver.com>
2026-09-12 01:10:53 +0200

    Reapply "remove legacy proxy scraper"
    This reverts commit 557c5de3b1d7c7772f5c717ecca299eb60ff5149.

--- backend/src/main/java/kr/ygh/maple/union/dto/raider/BlockPoint.java
+++
@@ -1,7 +0,0 @@
-package kr.ygh.maple.union.dto.raider;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record BlockPoint(long x, long y) {
-}
--- backend/src/main/java/kr/ygh/maple/union/dto/raider/Preset.java
+++
@@ -1,8 +0,0 @@
-package kr.ygh.maple.union.dto.raider;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record Preset(String[] union_raider_stat, String[] union_occupied_stat,
-                     Stat[] union_inner_stat, Block[] union_block) {
-}
